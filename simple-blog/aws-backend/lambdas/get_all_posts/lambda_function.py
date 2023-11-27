a632e4f4dab4d0e7d716145b286d4e23457574c4
Raphael Broesamle <raphael.broesamle@mhp.com>
2023-11-27 14:03:28 +0100

    Implemented the GET REST endpoint for the aws-backend.

--- simple-blog/aws-backend/lambdas/get_all_posts/lambda_function.py
+++ simple-blog/aws-backend/lambdas/get_all_posts/lambda_function.py
@@ -1,9 +1,17 @@
 import boto3
+import json
+import os
+
+TABLE_NAME = os.environ["DYNAMODB_TABLE_NAME"]
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(TABLE_NAME)
 
 
 def lambda_handler(event, context):
-    result = "Hello World"
+    response = table.scan()
+    items = response.get("Items")
+    posts = [i.get("post") for i in items]
     return {
         'statusCode': 200,
-        'body': result
+        'body': json.dumps(posts)
     }
